9aa0a6125c1d44d479e52d051578cd78b656da31
David Runge <dvzrv@archlinux.org>
2020-05-23 18:18:20 +0200

    Adding secure boot option for run_archiso.sh
    scripts/run_archiso.sh:
    Adding secure boot option (by using edk2-ovmf secure boot firmware based
    on upstream documentation:
    https://github.com/tianocore/edk2/blob/master/OvmfPkg/README).
    It seems the secure boot images don't work as expected though, as
    archiso is able to boot using them (and it shouldn't).

--- scripts/run_archiso.sh
+++ scripts/run_archiso.sh
@@ -21,6 +21,7 @@ Flags:
     -b              set boot type to 'bios' (default)
     -h              print help
     -i [image]      image to boot into
+    -s              use secure boot (only relevant when using UEFI)
     -u              set boot type to 'uefi'
     -w [work_dir]   directory to copy state files to ('work' by default)
 
@@ -67,15 +68,25 @@ run_image_using_bios() {
 }
 
 run_image_using_uefi() {
+    local ovmf_code=/usr/share/edk2-ovmf/x64/OVMF_CODE.fd
+    local secure_boot_state=off
     copy_ovmf_vars
+    if [ "${secure_boot}" == "yes" ]; then
+        echo "Using Secure Boot"
+        ovmf_code=/usr/share/edk2-ovmf/x64/OVMF_CODE.secboot.fd
+        secure_boot_state=on
+    fi
     qemu-system-x86_64 \
         -boot order=d,menu=on,reboot-timeout=5000 \
         -m size=3072,slots=0,maxmem=$((3072*1024*1024)) \
         -k en \
         -name archiso,process=archiso_0 \
         -drive file="${image}",media=cdrom,readonly=on \
-        -drive if=pflash,format=raw,readonly,file=/usr/share/edk2-ovmf/x64/OVMF_CODE.fd \
-        -drive if=pflash,format=raw,file="${work_dir}/OVMF_VARS.fd" \
+        -drive if=pflash,format=raw,unit=0,file="${ovmf_code}",readonly \
+        -drive if=pflash,format=raw,unit=1,file="${work_dir}/OVMF_VARS.fd" \
+        -machine type=q35,smm=on,accel=kvm \
+        -global driver=cfi.pflash01,property=secure,value="${secure_boot_state}" \
+        -global ICH9-LPC.disable_s3=1 \
         -display sdl \
         -vga virtio \
         -enable-kvm \
@@ -84,10 +95,11 @@ run_image_using_uefi() {
 
 image=""
 boot_type="bios"
+secure_boot="no"
 work_dir=work/
 
 if [ ${#@} -gt 0 ]; then
-    while getopts 'bhi:uw:' flag; do
+    while getopts 'bhi:suw:' flag; do
         case "${flag}" in
             b) boot_type=bios
                 ;;
@@ -104,6 +116,8 @@ if [ ${#@} -gt 0 ]; then
                 fi
                 ;;
             u) boot_type=uefi
+                ;;
+            s) secure_boot=yes
                 ;;
             w) work_dir=$OPTARG
                 if [ -z "$work_dir" ]; then
